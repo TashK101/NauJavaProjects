5b26e1f792f7e6a9c500ef532d941a844a0cacb8
TashK101 <fornatasha123@mail.ru>
2024-10-10 10:04:54 +0500

    Adress entity added

---
+++ src/main/java/ru/karimova/NauJava/repositories/CrudRepository.java
@@ -0,0 +1,12 @@
+package ru.karimova.NauJava.repositories;
+
+public interface CrudRepository<T, ID> {
+
+    void create(T entity);
+
+    T read(ID id);
+
+    void update(T entity);
+
+    void delete(ID id);
+}
